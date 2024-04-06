aafc7209e705927e00e77c24c556daca15afa7ef
kaavyakalani26 <kaavya.kalani@mail.utoronto.ca>
2024-04-06 03:06:01 -0400

    raw data added

--- scripts/01-download_data.R
+++
@@ -1,9 +0,0 @@
-#### Preamble ####
-# Purpose: Downloads and saves the data from XX
-# Author: Kaavya Kalani
-# Date: XX March 2024
-# Contact: kaavya.kalani@mail.utoronto.ca
-# License: MIT
-# Pre-requisites: 
-
-#### Workspace setup ####
--- scripts/05-poststratification.R
+++
@@ -1,9 +0,0 @@
-#### Preamble ####
-# Purpose: Poststratification for the model
-# Author: Kaavya Kalani
-# Date: XX March 2024
-# Contact: kaavya.kalani@mail.utoronto.ca
-# License: MIT
-# Pre-requisites: Run 04-model.R
-
-#### Workspace setup ####
